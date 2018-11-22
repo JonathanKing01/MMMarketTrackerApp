412b394e3e9a44a6968aec34dd9c89650c15a30b
unknown <jkin677@aucklanduni.ac.nz>
2018-11-22 17:29:05 +1300

    Added voice regonition to the item search bar

--- src/components/ItemSearch.tsx
+++ src/components/ItemSearch.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import MediaStreamRecorder from 'msr'
 
 import '../App.css';
 
@@ -15,6 +16,8 @@ export default class ItemSearch extends React.Component<IProps, {}> {
         super(props)
         this.selectNewItem = this.selectNewItem.bind(this);
         this.searchByName = this.searchByName.bind(this);
+        this.searchNameByVoice = this.searchNameByVoice.bind(this);
+        this.postAudio = this.postAudio.bind(this);
     }
 
     public render() {
@@ -23,10 +26,11 @@ export default class ItemSearch extends React.Component<IProps, {}> {
             <p className="Search-bar">
                 <input type="text" id="search-item-textbox" className="Search-box" placeholder="Search By Item Name" />
                 <div className="Search-button" onClick = {this.searchByName}>Search</div>
+                <div className="btn" onClick={this.searchNameByVoice}><i className="fa fa-microphone" /></div>
             </p>
 
             <div className="table-div">
-            <table className="table">
+            <table className="table"> 
                 <tbody>
                     {this.createTable()}
                 </tbody>
@@ -73,5 +77,67 @@ export default class ItemSearch extends React.Component<IProps, {}> {
         }
         const name = textBox.value 
         this.props.searchByName(name)  
+    }
+
+    private searchNameByVoice(){
+        const mediaConstraints = {
+            audio: true
+        }
+        const onMediaSuccess = (stream: any) => {
+            const mediaRecorder = new MediaStreamRecorder(stream);
+            mediaRecorder.mimeType = 'audio/wav'; // check this line for audio/wav
+            mediaRecorder.ondataavailable = (blob: any) => {
+                this.postAudio(blob);
+                mediaRecorder.stop()
+            }
+            mediaRecorder.start(3000);
+        }
+    
+        navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError)
+    
+        function onMediaError(e: any) {
+            console.error('media error', e);
+        }
+    }
+
+    private postAudio(blobo: any){
+        let accessToken: any;
+        fetch('https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken', {
+            headers: {
+                'Content-Length': '0',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Ocp-Apim-Subscription-Key': '538cabf19170421294c2b8772495198b'
+            },
+            method: 'POST'
+        }).then((response) => {
+            // console.log(response.text())
+            return response.text()
+        }).then((response) => {
+            alert("Token: " + response)
+            accessToken = response
+        }).catch((error) => {
+            console.log("Error", error)
+        });
+
+         // posting audio
+        fetch('https://westus.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=en-US.', {
+            body: blobo, // this is a .wav audio file    
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer' + accessToken,
+                'Content-Type': 'audio/wav;codec=audio/pcm; samplerate=16000',
+                'Ocp-Apim-Subscription-Key': '538cabf19170421294c2b8772495198b'
+            },    
+            method: 'POST'
+        }).then((res) => {
+            return res.json()
+        }).then((res: any) => {
+            console.log(res)
+            alert("Heard: " + (res.DisplayText as string).slice(0, -1))
+            const textBox = document.getElementById("search-item-textbox") as HTMLInputElement
+            textBox.value = (res.DisplayText as string).slice(0, -1)
+        }).catch((error) => {
+            console.log("Error", error)
+        });
     }
 }
